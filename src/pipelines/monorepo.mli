9922c9fe08c3f41c66376894f09c53dbee032b2d
Lucas Pluvinage <lucas@tarides.com>
2021-11-26 14:37:59 +0100

    decommission docs

--- src/pipelines/monorepo.mli
+++ src/pipelines/monorepo.mli
@@ -16,12 +16,6 @@ val lock :
 (** [lock ~system ~value ~monorepo ~repos projects] Obtain the lockfile of
     [projects] using the [monorepo] tool with the repositories [repos]. *)
 
-val docs :
-  system:Platform.system ->
-  repos:Repository.t list Current.t ->
-  lock:Monorepo_lock.t Current.t ->
-  unit Current.t
-
 val released :
   config:Config.t ->
   platform:Platform.t ->
